51d02cc69e625c03b416e06d4b21d8a7805935df
user <kozlova.valeriya.v@gmail.com>
2020-01-03 11:28:37 +0100

    socket.io

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,2 +1,24 @@
 // connect to sockets.io client side
-io();
+let socket = io.connect();
+let form = document.getElementById("messForm");
+let all_messages = document.getElementById("all_mess");
+let submit = document.getElementById("submit");
+
+submit.addEventListener('click', e => {
+    let inputText = document.getElementById("message").value;
+    let newName = document.getElementById("name").value;
+    event.preventDefault();
+    socket.emit('send mess', {mess: inputText, name: newName});
+    document.getElementById("message").value = "";
+});
+socket.on('add mess', function(data) {
+    let div = document.createElement("div");
+    let spanName = document.createElement("span");
+    spanName.innerHTML = data.name + ": ";
+    spanName.style.fontWeight = "bold";
+    let spanMess = document.createElement("span");
+    spanMess.innerHTML = data.msg;
+    all_messages.appendChild(div);
+    div.appendChild(spanName);
+    div.appendChild(spanMess);
+});
