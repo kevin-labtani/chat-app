7113e8f183993de8c6e0e103b57928ea6243a23f
Kevin Labtani <kevinlabtani@gmail.com>
2020-01-03 15:01:51 +0100

    refactor project architecture

--- src/index.js
+++ src/index.js
@@ -17,34 +17,34 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
 async function start() {
-    try {
-      // connect to db
-        await require("./db/mongoose");
-        // server.listen(PORT);
-        server.listen(PORT, console.log(`Server started on port ${PORT}`));
-        app.get('/chat', function(req, res) {
-          res.sendFile(path.join(__dirname, '../public', 'chat.html'));
-        });
-        
-        users = [];
-        connections = [];
-        
-        io.sockets.on('connection', function(socket) {
-            console.log("Success connection");
-            connections.push(socket);
-        
-            socket.on('disconnect', function(data) {
-            connections.splice(connections.indexOf(socket), 1);
-            console.log("Disconnection");
-            });
-            
-            socket.on('send mess', function(data) {
-            io.sockets.emit('add mess', {msg: data.mess, name: data.name});
-          });
-        });
-    } catch (e) {
-        console.log(e);
-    }
-};
+  try {
+    // connect to db
+    await require("./db/mongoose");
+    // server.listen(PORT);
+    server.listen(PORT, console.log(`Server started on port ${PORT}`));
+    app.get("/chat", function(req, res) {
+      res.sendFile(path.join(__dirname, "/views", "chat.html"));
+    });
+
+    users = [];
+    connections = [];
+
+    io.sockets.on("connection", function(socket) {
+      console.log("Success connection");
+      connections.push(socket);
+
+      socket.on("disconnect", function(data) {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("Disconnection");
+      });
+
+      socket.on("send mess", function(data) {
+        io.sockets.emit("add mess", { msg: data.mess, name: data.name });
+      });
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 start();
