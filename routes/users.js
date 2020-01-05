468e18a962a99f0c39d5a8cb6d13450ee4825d9f
Kevin Labtani <kevinlabtani@gmail.com>
2020-01-05 20:45:26 +0100

    validation for register post route

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,36 @@ router.get("/register", (req, res) => res.render("register"));
 
 // post register route
 router.post("/register", (req, res) => {
-  console.log(req.body);
-  res.send("hello");
+  const { name, email, password, password2 } = req.body;
+
+  let errors = [];
+
+  // check required
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "Please fill in all fields" });
+  }
+
+  // check pwd match
+  if (password !== password2) {
+    errors.push({ msg: "Passwords do not match" });
+  }
+
+  // check pwd length
+  if (password.length < 6) {
+    errors.push({ msg: "Password must be at least 6 characters" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    res.send("pass");
+  }
 });
 
 module.exports = router;
