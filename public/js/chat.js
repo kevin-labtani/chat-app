3706e6738aff37bb53c5eb2dca4079d0ce126545
Vincent Bouton <vincent.bouton.dev@gmail.com>
2020-01-08 11:49:49 +0100

    Add scrollbar + style on chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,20 +3,22 @@ let socket = io.connect();
 let form = document.getElementById("messForm");
 let all_messages = document.getElementById("all_mess");
 let submit = document.getElementById("submit");
-let newName =  document.getElementById("name").innerHTML.split(' ');
+let newName = document.getElementById("name").innerHTML.split(" ");
 
-socket.emit("send join", {name: newName.slice(1) });
+socket.emit("send join", { name: newName.slice(1) });
 socket.on("add join", function(data) {
   let div = document.createElement("div");
+  div.className = "border-bottom border-top";
+  div.style.height = "80px";
   let spanJoin = document.createElement("span");
-  spanJoin.innerHTML = data.name + " joined the chat!";
+  spanJoin.innerHTML = `It's a bird! It's a plane! Nevermind, it's just ${data.name}`;
   all_messages.appendChild(div);
   div.appendChild(spanJoin);
 });
 
 const logout = document.getElementById("logout");
-logout.addEventListener('click', e => {
-    socket.emit("send left", {name: newName.slice(1) });
+logout.addEventListener("click", e => {
+  socket.emit("send left", { name: newName.slice(1) });
 });
 socket.on("add left", function(data) {
   let div = document.createElement("div");
@@ -33,14 +35,15 @@ submit.addEventListener("click", e => {
   document.getElementById("message").value = "";
 });
 socket.on("add mess", function(data) {
-  let div = document.createElement("div");
-  let spanName = document.createElement("span");
+  const div = document.createElement("div");
+  const spanName = document.createElement("span");
+  div.className = "border-bottom border-top";
+  div.style.height = "80px";
   spanName.innerHTML = data.name + ": ";
   spanName.style.fontWeight = "bold";
-  let spanMess = document.createElement("span");
+  const spanMess = document.createElement("span");
   spanMess.innerHTML = data.msg;
   all_messages.appendChild(div);
   div.appendChild(spanName);
   div.appendChild(spanMess);
-  
 });
